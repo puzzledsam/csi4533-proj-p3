0bd851375b58da50bcabceafb08f62ee4d9b3e76
Samuel Dion <samuelqwe@icloud.com>
2024-03-22 10:41:59 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+from PIL import Image
+from utils import model, tools
+import torch
+
+# Point d'entrée principal du script
+if __name__ == "__main__":
+    
+    torch_device_name = "cpu"
+    if torch.backends.mps.is_available():
+        # Pour les Macs avec Apple Silicon
+        torch_device_name = "mps"
+    elif torch.cuda.is_available():
+        # Pour les GPU Nvidia
+        torch_device_name = "cuda"
+        
+    torch_device = torch.device(torch_device_name)
+    print(f"Using {torch_device} torch device for inference")
+
+    # Définir les répertoires source et de sortie
+    source_path_dir = "images"
+    output_path_dir = "output"
+    #image_name = "sample_2.png"
+
+    # Charger le modèle et appliquer les transformations à l'image
+    seg_model, transforms = model.get_model()
+
+    # Ouvrir les images et appliquer les transformations
+    for root, dirs, _ in os.walk(source_path_dir):
+        for subdir in dirs:
+            full_dir = os.path.join(root, subdir)
+            for file_name in os.listdir(full_dir):
+                image_path = os.path.join(full_dir, file_name)
+                image = Image.open(image_path)
+                transformed_img = transforms(image)
+                
+
+                # Effectuer l'inférence sur l'image transformée sans calculer les gradients
+                with torch.no_grad():
+                    output = seg_model([transformed_img])
+
+                # Traiter le résultat de l'inférence
+                result = tools.process_inference(output,image)
+                output_dir_full_path = os.path.join(output_path_dir, subdir)
+                if not os.path.exists(output_dir_full_path):
+                    os.makedirs(output_dir_full_path)
+                result.save(os.path.join(output_dir_full_path, file_name))
+                # result.show()
---
+++ utils/tools.py
@@ -0,0 +1,52 @@
+import numpy as np
+from utils.config import THRESHOLD, PERSON_LABEL, MASK_COLOR, ALPHA
+from PIL import Image
+
+# Fonction pour traiter les sorties d'inférence du modèle
+def process_inference(model_output, image):
+
+    np_masks = [] # Pour stocker les masques détectés
+
+    # Extraire les masques, les scores, et les labels de la sortie du modèle
+    masks = model_output[0]['masks']
+    scores = model_output[0]['scores']
+    labels = model_output[0]['labels']
+
+    # Convertir l'image en tableau numpy
+    img_np = np.array(image)
+
+    # Parcourir chaque prédiction pour appliquer le seuil et le label
+    for i, (mask, score, label) in enumerate(zip(masks, scores, labels)):
+    
+        # Appliquer le seuil et vérifier si le label correspond à une personne
+        if score > THRESHOLD and label == PERSON_LABEL:
+            
+            # Convertir le masque en tableau numpy et l'appliquer à l'image            
+            mask_np = mask[0].mul(255).byte().cpu().numpy() > (THRESHOLD * 255)    
+            np_masks.append(mask_np)
+                    
+            for c in range(3):
+                img_np[:, :, c] = np.where(mask_np, 
+                                        (ALPHA * MASK_COLOR[c] + (1 - ALPHA) * img_np[:, :, c]),
+                                        img_np[:, :, c])
+                
+    with open('output/saved_masks.npy', 'wb') as f:
+        np.save(f,np_masks)
+    
+    # Convertir en image à partir d'un tableau numpy et le renvoyer            
+    return Image.fromarray(img_np.astype(np.uint8))
+
+def apply_saved_mask(image):
+
+    # Convertir l'image en tableau numpy
+    img_np = np.array(image)
+    masks = np.load('examples/output/saved_masks.npy')
+    # Parcourir chaque prédiction pour appliquer le seuil et le label
+    for i, mask in enumerate(masks):  
+        for c in range(3):
+            img_np[:, :, c] = np.where(mask, 
+                                    (ALPHA * MASK_COLOR[c] + (1 - ALPHA) * img_np[:, :, c]),
+                                    img_np[:, :, c])
+    
+    # Convertir en image à partir d'un tableau numpy et le renvoyer            
+    return Image.fromarray(img_np.astype(np.uint8))
