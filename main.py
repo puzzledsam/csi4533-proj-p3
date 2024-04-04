7119798da3e44ac67b61e2ce91d18fc3ca5d87c4
Samuel Dion <samuelqwe@icloud.com>
2024-04-04 19:41:44 -0400

    Start combining part 1 and 2

--- main.py
+++ main.py
@@ -1,9 +1,60 @@
+import cv2
+import numpy as np
 import os
 from PIL import Image
 from utils import model, tools
 import torch
 
-# Point d'entrée principal du script
+### FONCTIONS ###
+
+def getPersonHistograms(source_img, person_rect):
+    '''
+        Retourne l'histogramme complet et le demi-histogramme
+        pour la personne délimitée par le rectangle dans l'image source.
+    '''
+    
+    # Obtenir les masques pour une personne et sa moitié supérieure
+    masks = []
+    for i in [1,2]:
+        x, y, w, h = person_rect
+        
+        if h < 40 or w < 25:
+            # On veut ignorer les petits régions
+            break
+        
+        h = int(h / i) # Première iteration prends le masque complet, le deuxième ne prends que la moitiée supérieure
+        
+        cv2.rectangle(source_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        mask = np.zeros(source_img.shape[:2], dtype=np.uint8)
+        cv2.rectangle(mask, (x, y), (x+w, y+h), (255), -1)
+        masks.append(mask)
+        
+    # Calculer histogrammes pour chaque masque
+    histograms = []
+    for mask in masks:
+        hist = cv2.calcHist([source_img], [0, 1, 2], mask, [25] * 3, [0, 256] * 3)
+        hist = cv2.normalize(hist, hist).flatten()
+        histograms.append(hist)
+        
+    
+    return histograms
+
+def comparePersonHistograms(person1, person2):
+    '''
+        Comparer jusqu'à 4 histogrammes ensemble (jusqu'à 2 par personne) pour
+        trouver un score de correspondance et renvoyer le score maximum.
+    '''
+    
+    scores = []
+    for i in range(len(person1)):
+        for j in range(len(person2)):
+            # Compare chaque combination des histogrammes avec la méthode d'intersection
+            scores.append(cv2.compareHist(person1[i], person2[j], cv2.HISTCMP_INTERSECT))
+    
+    return max(scores, default=None)
+
+### Point d'entrée principal du script ###
+
 if __name__ == "__main__":
     
     torch_device_name = "cpu"
